b010b5eeb0e66017d1df13edad5c89d99c5a5219
anantNeelkanthi <anantkumarneelkanthi@gmail.co>
2024-08-30 11:41:12 +0530

    project completed

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React from 'react'
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+function page() {
+  return (
+    <div className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold text-white">
+         Contact Us
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          We&apos;re here to help with any questions about our courses,
+          programs, or events. Reach out and let us know how we can assist you
+          in your musical journey.
+        </p>
+        <input
+          type="text"
+          placeholder="Your email address"
+          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700 p-5"
+        />
+
+        <textarea name="Your message" placeholder="Your message" className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700 p-5"></textarea>
+      </div>
+      <BackgroundBeams />
+    </div>
+  )
+}
+
+export default page
